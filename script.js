bf60fc793b9ea23c66d1677d2933252b4cd79570
kskannandotwin <kskannan.win@gmail.com>
2024-02-26 22:50:12 +0530

    26.02.2024-pvoip
    completed

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 'use strict';
 
-let age = 30;
-let oldAge = age;
-age = 31;
-console.log(age);
-console.log(oldAge);
+// primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
 
-const me = {
-  name: 'Jonas',
-  age: 30
+// reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27
 };
 
-const friend = me;
-friend.age = 27;
-console.log('Friend:', friend );
-console.log('Me:', me );
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before married ', jessica);
+console.log('After married ', marriedJessica);
+// marriedJessica = {};
+
+// copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob']
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+console.log('Before married ', jessica2);
+console.log('After married ', jessicaCopy);
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before married ', jessica2);
+console.log('After married ', jessicaCopy);
