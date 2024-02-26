d07fa8648b08ebe740b83766931318f77ff6552e
kskannandotwin <kskannan.win@gmail.com>
2024-02-26 06:20:06 +0530

    26.02.2024-hatip
    completed

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// variables
 
-  function printAge() {
-    let output = `${firstName}, you are ${age} and born in ${birthYear}.`;
-    console.log(output);
+console.log(me);
+// console.log(job);
+// console.log(year);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      //   creating new variable with same name as outer scope's variable
-      const firstName = 'Stevem';
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
 
-      //   re-assigning outer scope's variale
-      output = 'New Output';
+// functions
 
-      const str = `Oh, and you are a millenial, ${firstName}`;
-      console.log(str);
+console.log(addDecl(2, 3));
+// console.log(addExp(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    console.log(millenial);
-    // console.log(add(2, 3));
-    console.log(output);
-  }
-  printAge();
-
-  return age;
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
-// console.log(age);
-// printAge();
+const addExp = function(a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// example
+
+console.log(undefined);
+if(!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
